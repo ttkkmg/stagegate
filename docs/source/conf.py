52695d91d6ccaf22f1553d3749097afb4ef838ae
ttkkmg <tk@fermlab.com>
2026-03-24 22:51:42 +0900

    delete unused import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,8 +5,6 @@ from __future__ import annotations
 import sys
 from pathlib import Path
 
-import sphinx_rtd_theme
-
 ROOT = Path(__file__).resolve().parents[2]
 SRC = ROOT / "src"
 sys.path.insert(0, str(SRC))
